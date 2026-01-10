3575c2c60d0d312eae6100f7b320fbd3ece1c997
ernestweems <ernestweems@gmail.com>
2026-01-09 23:43:35 -0500

    exercise complete input issue fix

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { range } from '../../utils';
 
 
-function Guess({value}) {
+function Guess({value}) { 
+   const word = value?.value;
   return (
-
   <p className='guess'>
-      {range(5).map((num) => (
-        <span key={num} className='cell'>
-          {value ? value[num] : undefined}
-        </span>
+      {range(5).map((obj) => (
+        <span key={obj.id} className='cell'>
+          {word ? word[obj] : undefined}
+          </span>
       ))}
   </p>
 
