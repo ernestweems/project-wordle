8dd578c583d8e3ab24ac8672d115186f918bc797
ernestweems <ernestweems@gmail.com>
2026-01-11 13:56:55 -0500

    Exercise 5: complete used given solution

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
-function GuessInput({ handleSubmitGuess }) {
+function GuessInput({ gameStatus,  handleSubmitGuess }) {
   const [tentativeGuess, setTentativeGuess] = React.useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
-
     handleSubmitGuess(tentativeGuess);
-
     setTentativeGuess('');
   }
-
   return (
     <form onSubmit={handleSubmit} className="guess-input-wrapper">
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         required
+        disabled={gameStatus !== 'running'}
         minLength={5}
         maxLength={5}
         pattern="[a-zA-Z]{5}"
