5933a626c7cef2b870799058a224d61443faecbc
ernestweems <ernestweems@gmail.com>
2026-01-11 12:12:14 -0500

    exsrcise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers'
 
-function Guess({ value }) {
+function Cell({ letter, status}){
+
+  const className = status ? `cell ${status}` : 'cell';
+
+  // or use this ---> ${status ?? ''} other is more readable
+   return (
+     <span className={className}>
+          {letter}
+        </span>
+   )  
+}
+
+function Guess({ value, answer }) {
+  const result = checkGuess( value, answer)
+  console.log({result})
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
+        <Cell key={num}
+        letter={result ? result[num].letter : undefined}
+        status={result ? result[num].status : undefined}
+      />
       ))}
     </p>
   );
